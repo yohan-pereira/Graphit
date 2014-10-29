b0af565dda0903e348b90dd77adf6eb43710781d
Takeshi Nakano <tf0054@gmail.com>
2014-10-29 13:10:27 +0100

    Fixed.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject graphit "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-		 [org.clojars.marktriggs/jfreechart "1.0.13"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+		 [org.clojars.marktriggs/jfreechart "1.0.13"]
+		 [org.clojure/tools.cli "0.3.1"]]
   :namespaces :all
   :aot [graphit]
   :main graphit)
